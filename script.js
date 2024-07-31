df99889bbf922258fcfe1f9efca6661ed95ac0e0
akshaydinesh2003 <72128576+akshaydinesh2003@users.noreply.github.com>
2024-07-31 13:50:59 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function scrollDown() {
+    document.getElementById('about').scrollIntoView({ behavior: 'smooth' });
+}
+
+document.querySelectorAll('nav a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
